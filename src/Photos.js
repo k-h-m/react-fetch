14ef15da513cc307d1fe1e593964fc83a93fcf3d
k-h-m <misha.kharlamov@gmail.com>
2019-02-08 11:51:03 +0300

    fetch data with hooks

---
+++ src/Photos.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useFetch } from "./hooks";
+
+function Photos() {
+  const [data, loading] = useFetch(
+    "https://jsonplaceholder.typicode.com/photos?albumId=1"
+  );
+  return (
+    <>
+      <h1>Photos</h1>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <ul>
+          {data.map(({ id, title, url }) => (
+            <li key={`photo-${id}`}>
+              <img alt={title} src={url} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+export default Photos;
