e39f137b66b9f825bac8b11d28b65f7d55ef5041
k-h-m <misha.kharlamov@gmail.com>
2019-07-07 00:20:16 +0300

    new style for item list

--- src/Photos.js
+++ src/Photos.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { useFetch } from "./hooks";
+import "./Photos.css"
 
 function Photos() {
   const [data, loading] = useFetch(
-    "https://jsonplaceholder.typicode.com/photos?albumId=1"
+    "/api/10?q=1"
   );
+  var cardStyle = {
+   height: 800,
+   width: 400,
+   padding: 0,
+   backgroundColor: "#FFF",
+   WebkitFilter: "drop-shadow(0px 0px 5px #666)",
+   filter: "drop-shadow(0px 0px 5px #666)"
+  };
   return (
     <>
-      <h1>Photos</h1>
       {loading ? (
         "Loading..."
       ) : (
-        <ul>
-          {data.map(({ id, title, url }) => (
-            <li key={`photo-${id}`}>
-              <img alt={title} src={url} />
+        <ul className="book_list">
+              {data.map(({ id, title, publisher, year, author, coverurl, language }) => (
+		    <li key={`photo-${id}`} className="tile">
+		    <div className="descr">
+		      <h1>{title}</h1>
+		      <ul>
+		      <li>{id}</li>
+		      <li>{author}</li>
+		      <li>{publisher}({year})</li>
+		      <li>{language}</li>
+		      </ul>
+		      <ul className="tags">
+		      <li>dd</li>
+		      <li>geometry</li>
+		      <li>statistics</li>
+		      <li>combinatorics</li>
+		      <li>physics</li>
+		      <li>CS</li>
+		      <li>junk</li>
+		      </ul>
+		</div>
+		    <div className="book_img">
+		    <img src={coverurl} />
+		    </div>
             </li>
           ))}
         </ul>
